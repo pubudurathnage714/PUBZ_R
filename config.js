d36de252faa8ffbee91c678fec2bb8313e2f1514
pubudurathnage714 <pubudurathnage714@gmail.com>
2025-12-18 16:50:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/pubudurathnage714/bot_helper/blob/main/Images/Gemini_Generated_Image_h2ns7ch2ns7ch2ns.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 AURO_MD Is Alive Now😍*",
+BOT_OWNER: '94752997591',  // Replace with the owner's phone number
 
 
 
